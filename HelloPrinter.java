5db5d89693f4461717aa17d8a73e35aa4bb74779
BrodyBoell <143550750+BrodyBoell@users.noreply.github.com>
2023-08-30 12:21:15 -0500

    Add files via upload

---
+++ HelloPrinter.java
@@ -0,0 +1,13 @@
+
+/**
+*Brody Boell - bjboell1
+*CIS171 22149
+*Jan 17, 2023
+*/
+public class HelloPrinter {
+	public static void main(String[] args) {
+		
+	
+		System.out.println("Hello World");
+	}
+}
