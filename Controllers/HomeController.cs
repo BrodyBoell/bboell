8e8a82402bd8869720fdd7c3b293d002ca039f74
BrodyBoell <143550750+BrodyBoell@users.noreply.github.com>
2024-01-17 14:04:30 -0600

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResponsiveWebApp.Models;
+using System.Diagnostics;
+
+namespace ResponsiveWebApp.Controllers
+{
+	public class HomeController : Controller
+	{
+
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			//Setting default values
+
+			ViewBag.currentAge = 0;
+			ViewBag.print = "";
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Index(AgeThisYear model)
+		{
+			if (ModelState.IsValid)
+			{
+				//Showing the data from methods
+
+				ViewBag.currentAge = model.ageThisYear();
+				ViewBag.print = model.Output();
+			}
+			else
+			{
+				ViewBag.currentAge = 0;
+			}
+			return View(model);
+		}
+	}
+}
---
+++ Models/AgeThisYear.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ResponsiveWebApp.Models
+{
+	public class AgeThisYear
+	{
+		//input validation
+		[Required(ErrorMessage = "Please enter a Name.")]
+		[RegularExpression(@"^(([A-za-z]+[\s]{1}[A-za-z]+)|([A-Za-z]+))$", ErrorMessage ="Entry must be Full Name")]
+		public String? Name { get; set; }
+		//input validation
+		[Required(ErrorMessage = "Please enter a Birth Year.")]
+		[Range(1900, 2100, ErrorMessage = "Enter a Birth Year between 1900 and 2100.")]
+		public int? BirthYear { get; set; }
+		public String? currentAge { get; set; }
+		//Method to find age
+		public int? ageThisYear()
+		{
+			int? year = 2024;
+			 int? currentAge = year - BirthYear;
+			
+			return currentAge;
+		}
+		//Method to print message of how old someone will be on December 31st
+
+		public string? Output()
+		{
+			
+			string? print = Convert.ToString(Name + " will be " + currentAge + " On December 31st");
+			return print;
+		}
+	}
+}
